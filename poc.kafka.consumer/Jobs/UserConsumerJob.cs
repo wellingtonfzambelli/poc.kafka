518462f36e0c950799671ba9b6f36a14e1c3216f
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2025-01-05 12:36:04 -0300

    backgroundservice working

---
+++ poc.kafka.consumer/Jobs/UserConsumerJob.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace poc.kafka.consumer.Jobs;
+
+internal sealed class UserConsumerJob : BackgroundService
+{
+    private readonly ILogger<UserConsumerJob> _logger;
+
+    public UserConsumerJob(ILogger<UserConsumerJob> logger) =>
+        _logger = logger;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("UserConsumerJob started in background.");
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation($"UserConsumerJob task started at {DateTime.Now}");
+
+            await Task.Delay(5000, stoppingToken); // Simulating some work
+        }
+
+        _logger.LogInformation("UserConsumerJob service was cancelled.");
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Application has finished");
+        return Task.CompletedTask;
+    }
+}
--- poc.kafka.consumer/Program.cs
+++ poc.kafka.consumer/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("hjh");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using poc.kafka.consumer.Jobs;
+using poc.kafka.crosscutting.Kafka;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddHostedService<UserConsumerJob>();
+builder.Services.AddTransient<IUserProducer, UserProducer>();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+using var host = builder.Build();
+await host.RunAsync();
